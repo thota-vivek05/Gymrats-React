0cc67235c5d27df52cbe8917fab6883a44107bdb
thota-vivek05 <thota.vivek2005@gmail.com>
2026-02-05 18:37:34 +0530

    addedd 404 not found page

---
+++ Frontend/src/pages/extra/NotFound.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const NotFound = () => {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+      <Link to="/" style={{ color: 'blue', textDecoration: 'underline' }}>
+        Go back to Home
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
